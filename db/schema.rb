9b45539bf073e97cd43f629d746f80f9dba3dc49
dominiccodes <email@domcodes.com>
2017-08-08 04:58:16 +0100

    Adds fields for file upload functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170808034548) do
+ActiveRecord::Schema.define(version: 20170808035526) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -18,8 +18,12 @@ ActiveRecord::Schema.define(version: 20170808034548) do
     t.string   "director"
     t.string   "rating"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "poster_file_name"
+    t.string   "poster_content_type"
+    t.integer  "poster_file_size"
+    t.datetime "poster_updated_at"
   end
 
 end
